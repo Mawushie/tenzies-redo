1a5efd31b05f8ff59df4555a0e7432190111c845
Mawushie <afi.mawushie.honyah@gmail.com>
2024-08-09 20:12:01 +0000

    holdDice props received on Die component

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default function Die({ value, isHeld }) {
+export default function Die({ value, isHeld, holdDice }) {
   //conditionally rendering class of held dice based on isHeld property coming from props
   return (
     <div
       className={`flex justify-center items-center w-9 h-9 border rounded bg-white text-blue-950 font-bold shadow-md ${
         isHeld ? "heldDice" : ""
       }`}
+      onClick={holdDice}
     >
       {value}
     </div>
