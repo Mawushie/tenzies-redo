a8827334e3ace21e8e42dfe0f861f19f257308c8
Mawushie <afi.mawushie.honyah@gmail.com>
2024-08-09 21:25:20 +0000

    generate new dice function added, code refactored

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,68 @@ import "./App.css";
 import Die from "./components/Die";
 
 function App() {
-  const [dice, setDice] = useState([]);
-  //function that returns an array of 10 random numbers between 1-6 inclusive
+  //function to generate new Dice from 1 to 6 inclusive
+  const generateNewDice = () => {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: uuid(),
+    };
+  };
+
+  //function that generates an array of 10 die elements
   const allNewDice = () => {
     let diceArray = [];
     for (let i = 0; i < 10; i++) {
-      let randomNumber = Math.ceil(Math.random() * 6);
-      // let randomNumber = Math.floor(Math.random() * 6) + 1;
-      diceArray.push({
-        value: randomNumber,
-        isHeld: false,
-        id: uuid(),
-      });
+      diceArray.push(generateNewDice());
     }
-    // console.log(diceArray);
     return diceArray;
+  };
+  const [dice, setDice] = useState(allNewDice());
+
+  const rollDice = () => {
+    // setDice(allNewDice());
+    setDice((oldDice) => {
+      return oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDice();
+      });
+    });
   };
 
   //function to hold dice when clicked
-  const holdDice = () => {
-    //onClick of dice, change bg color to green, set isHeld to true
-    setDice((prevDice) => {});
-  };
-
-  //function to display dice elements
-  const generateDiceElements = () => {
-    let diceElements = allNewDice().map((item, index) => {
-      return <Die value={item.value} isHeld={item.isHeld} key={index} />;
+  const holdDice = (id) => {
+    console.log(id);
+    //setDice, map through array, check if id received is the same as id,
+    //if same, toggle is held else keep same
+    setDice((oldDice) => {
+      return oldDice.map((die) => {
+        return die.id === id
+          ? {
+              ...die,
+              isHeld: !die.isHeld,
+            }
+          : die;
+      });
     });
-    setDice(diceElements);
-    return diceElements;
   };
 
-  useEffect(() => {
-    console.log("rolled");
-    generateDiceElements();
-  }, []);
+  //displaying dice elements
+  const diceElements = dice.map((item) => {
+    return (
+      <Die
+        value={item.value}
+        isHeld={item.isHeld}
+        key={item.id}
+        holdDice={() => holdDice(item.id)}
+      />
+    );
+  });
 
   return (
     <>
       <main className="border rounded bg-slate-800 m-auto px-6 py-9 max-w-3xl">
         <div className="border rounded-md flex flex-col gap-7 justify-center items-center w-full h-96 bg-zinc-100">
-          <div className="w-72">
+          <div className="w-3/4 sm:w-72">
             <h1 className="text-black font-bold text-xl">Tenzies</h1>
             <p className="text-indigo-950">
               Roll until all dice are the same. Click each die to freeze it at
@@ -53,11 +73,13 @@ function App() {
             </p>
           </div>
 
-          <div className="grid grid-cols-5 grid-rows-2 gap-4">{dice}</div>
+          <div className="grid grid-cols-5 grid-rows-2 gap-4">
+            {diceElements}
+          </div>
 
           <button
             className="border rounded bg-blue-600 text-white font-bold w-24 h-9 cursor-pointer"
-            onClick={generateDiceElements}
+            onClick={rollDice}
           >
             Roll
           </button>
