977f499d4572c4f974b6f153a626de55bb08087a
Mawushie <afi.mawushie.honyah@gmail.com>
2024-08-09 19:16:38 +0000

    dice elements generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Die from "./components/Die";
 
 function App() {
+  const [dice, setDice] = useState([]);
+  //function that returns an array of 10 random numbers between 1-6 inclusive
+  const allNewDice = () => {
+    let diceArray = [];
+    for (let i = 0; i < 10; i++) {
+      let randomNumber = Math.floor(Math.random() * 6) + 1;
+      diceArray.push(randomNumber);
+    }
+    return diceArray;
+  };
+
+  //function to display dice elements
+  const generateDiceElements = () => {
+    let diceElements = allNewDice().map((item, index) => {
+      return <Die value={item} key={index} />;
+    });
+    setDice(diceElements);
+    return diceElements;
+  };
+
+  useEffect(() => {
+    generateDiceElements();
+  }, []);
+
   return (
     <>
-      <main className="justw-3/4 border rounded bg-slate-800 m-auto px-6 py-9 max-w-3xl ">
-        <div className=" flex justify-center items-center w-full h-80 bg-zinc-100">
-          <div className="grid grid-cols-5 grid-rows-2 gap-4">
-            <Die value={1} />
-            <Die value={1} /> <Die value={1} /> <Die value={1} />{" "}
-            <Die value={1} /> <Die value={1} /> <Die value={1} />{" "}
-            <Die value={1} /> <Die value={1} /> <Die value={1} />
+      <main className="border rounded bg-slate-800 m-auto px-6 py-9 max-w-3xl">
+        <div className="border rounded-md flex flex-col gap-7 justify-center items-center w-full h-96 bg-zinc-100">
+          <div className="w-72">
+            <h1 className="text-black font-bold text-xl">Tenzies</h1>
+            <p className="text-indigo-950">
+              Roll until all dice are the same. Click each die to freeze it at
+              its current value between rolls.
+            </p>
           </div>
+
+          <div className="grid grid-cols-5 grid-rows-2 gap-4">{dice}</div>
+
+          <button
+            className="border rounded bg-blue-600 text-white font-bold w-24 h-9"
+            onClick={generateDiceElements}
+          >
+            Roll
+          </button>
         </div>
       </main>
     </>
