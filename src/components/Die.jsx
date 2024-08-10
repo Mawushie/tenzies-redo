de95d2fc5ac6195f7b06baa48b55047352f95b92
Mawushie <afi.mawushie.honyah@gmail.com>
2024-08-10 18:28:24 +0000

    Die component refactored so that the die values reflect dice faces with dots

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import One from "./One";
+import Two from "./Two";
+import Three from "./Three";
+import Four from "./Four";
+import Five from "./Five";
+import Six from "./Six";
 
 export default function Die({ value, isHeld, holdDice }) {
   //conditionally rendering class of held dice based on isHeld property coming from props
   return (
     <div
-      className={`flex justify-center items-center w-9 h-9 border-none rounded bg-white text-blue-950 font-bold shadow-md ${
+      className={`flex justify-center items-center w-9 h-9 border none rounded bg-white text-blue-950 font-bold shadow-md ${
         isHeld ? "heldDice" : ""
       }`}
       onClick={holdDice}
     >
-      {value}
+      {value === 1 && <One />}
+      {value === 2 && <Two />}
+      {value === 3 && <Three />}
+      {value === 4 && <Four />}
+      {value === 5 && <Five />}
+      {value === 6 && <Six />}
     </div>
   );
 }
--- src/components/DieContainer.jsx
+++ src/components/DieContainer.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 export default function DieContainer({ children }) {
   return (
-    <div className="flex flex-col justify-between w-9 h-9 border none rounded bg-white text-blue-950 font-bold shadow-md p-0.5">
-      {children}
-    </div>
+    <div className="flex flex-col justify-between w-9 h-9 p-1">{children}</div>
   );
 }
